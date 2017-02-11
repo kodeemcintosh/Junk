a3f6ef999574c97ad77973d9e99ca2c628b7e358
234kw22i903k <234kw22i903k@234kw22i903k-2.local>
2017-02-11 14:26:25 -0500

    Added a bunch of kFlags stuff in order to test on this Junk Project

--- junk.go
+++ junk.go
@@ -3,15 +3,15 @@ package main
 import(
 	"os"
 	"fmt"
-        "github.com/kvmac/Junk/flags.go"
 )
 
 func main() {
 	
+	kFlags.initializer(args, junkDesc)
 	moveJunk()
 
 }
-flags.initializer(args, junkDesc)
+
 args := os.Args
 cliArg := os.Args[0]
 currDir := os.Getwd()
@@ -42,38 +42,172 @@ func moveJunk() {
 
 args := os.Args
 argsLen := len(args)
+
 initializer(args, argsLen)
 
-func initializer(argsArray []string, num int) {
-        if (num > 4) {
-                Println("Error: There should be no more than four arguments")
-        }
-        else if (num == 4) {
-                argFour := os.Args[3]
-        }
-        else if(num == 3) {
-                argThree := os.Args[2]
-        }
-        else if (num == 2) {
-                argTwo := os.Args[1]
-        }
-        else if (num == 1) {
-                argOne := os.Args[0]
-        }
-        else {
-                appOverview()
+//--------------------------------------------------- Dependencies: flagFilter, fmt
+var argOne, argTwo, argThree, argFour string
+// var fnOne, fnTwo, fnThree, fnFour func()
+
+func Initializer(argsArray []string) {
+
+	num := len(argsArray)
+
+	if num > 5 {
+                
+		fmt.Println("Error: There should be no more than four arguments")
+	} else if num == 5 {
+                
+		argOne, argTwo, argThree, argFour = os.Args[1], os.Args[2], os.Args[3], os.Args[4]
+                
+                flagFilter(m, argOne, argTwo, argThree, argFour)
+	} else if num == 4 {
+                
+		argOne, argTwo, argThree, argFour = os.Args[1], os.Args[2], os.Args[3], ""
+                
+                flagFilter(m, argOne, argTwo, argThree)
+	} else if num == 3 {
+                
+		argOne, argTwo, argThree, argFour = os.Args[1], os.Args[2], "", ""
+
+                flagFilter(argOne, argTwo)                
+	} else if num == 2 {
+                
+		argOne, argTwo, argThree, argFour = os.Args[1], "", "", ""
+
+                flagFilter(m, argOne)
+	} else {
+                
+		argOne, argTwo, argThree, argFour = "", "", "", ""
+
+                fmt.Println(desc)
+	}
+}
+//------------------------------------------------------------------------------------
+
+
+
+
+//------------------------------------------------------------------------------------ Dependencies: blankFilter
+var fn []func(){ fnOne, fnTwo, fnThree, fnFour}
+fnOne, fnTwo, fnThree, fnFour := fn[0], fn[1], fn[2], fn[3]
+
+func flagFilter(fMap map[string]func(), args ...string) ([]string) {
+        
+        blankFilter(args) 
+        
+        // iterates through and creates a byte slice for each argument 
+        for x := 0; x <= len(args); x++ {
+                var flag string
+                argSlice := []byte(args[x])
+                
+                // iterates through the length of each arg slice in order to compare string to flags
+                for i := 0; i <= len(argSlice); i++ {
+                        var flagSlice []byte
+                        
+                        flagSlice[i] = argSlice[i] 
+                        
+                        // iterates through the flag/function map 
+                        for _, k, v := range fMap {
+                                if flagSlice == k {
+                                        flag = k
+                                        break
+                                }
+                        }
+                        // checks whether flag has been set or not and then assigns the function based upon the flag
+                        if flag != "" {
+                               var param []byte
+                               a := 0
+                               
+                               if flagSlice != "=" || flagSlice != " " {
+                                       param[a] = flagSlice[i]
+                                       a++
+                               }
+                               if i == argSlice {
+                                        //would like to accept arguments here in version 2
+                                        // fn[x] = ExecuteFlag(param)
+                                        fn[x] = ExecuteFlag(flag)
+                                        return fn[x]
+                                       
+                               }
+                        }
+                }
         }
 }
+//---------------------------------------------------------------------
 
-func appOverview() {
 
-Println("This is the Junk command line tool")
-Println("Never rm -rf another directory into oblivion, again!")
-Println("Use the argument '--help' or '-h' to obtain a list of options")
+// similar to BuildFlag, but it sets a function to be executed without a flag
+//--------------------------------------------------------------------------- Dependencies: defaultFlagCheck
+var iFnMap = make(map[string]func(int))
+var sFnMap = make(map[string]func(string))
+
+var intDefault bool
+func DefaultFlagInt(i int, fn func(int)) {
+        intDefault = true
+        defaultFlagCheck()
 
 }
 
-func filterArg() (string) {
-
-
+var stringDefault bool
+func DefaultFlagString(s string, fn func(string)) {
+        stringDefault = true
+        defaultFlagCheck()
+        
 }
+//--------------------------------------------------------------------
+
+
+// Throws an error if there is more than one default flag set
+//---------------------------------------------------------------------------
+func defaultFlagCheck(err error) error{
+        
+        if intDefault && stringDefault {
+                return err
+        } else {
+                
+        }
+}
+//-------------------------------------------------------------------------
+
+
+
+
+//BuildFlag variables
+// Builds flag for
+//--------------------------------------------------------------------- Dependencies: N/A
+var m = make(map[string]func())
+
+func BuildFlag(flag string, fn func()) {
+	
+	m[flag] = fn
+}
+//---------------------------------------------------------------------
+
+
+
+
+// Gets the function corresponding to the flag input
+// not sure if I want this to be a public function
+//-------------------------------------------------------------------- Dependencies: N/A
+func ExecuteFlag(flag string) func(){
+
+	fn := m[flag]
+	return fn
+}
+//---------------------------------------------------------------------
+
+
+
+
+
+// iterates through arguments to make sure none are empty
+//-------------------------------------------------------------------- Dependencies: fmt
+func blankFilter(s []string) {
+        for _, x := range s {
+                if x == "" {
+                        fmt.Println(desc)
+                }
+        }
+}
+//-----------------------------------------------------------------------
