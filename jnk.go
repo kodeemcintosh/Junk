f612d6632df04f02281d52dde18847c19e08f21c
kwik <kodee.mcintosh@gmail.com>
2017-05-06 16:04:39 -0400

    Added file/directory iteration and refactored the code

--- jnk.go
+++ jnk.go
@@ -1,50 +1,68 @@
 package main
 
-import(
-        // "bytes"
+import (
 	"fmt"
 	"os"
-        // "path/filepath"
 )
 
 func main() {
-	
-        arg := os.Args[1]
-                
-        junkDesc := "This is the Junk Command Line Tool /new Never 'rm -rf' your files or directories into oblivion ever again! /new Enter '--help' or '-h' for a list of options"
-        
-        if arg != "" {
-                
-                currDir, err := os.Getwd()
-                homeDir, hasHomeDir := os.LookupEnv("HOME")
-                junkDir := fmt.Sprint(homeDir + "/.Junk/")
-                
-                if err != nil {
-                        fmt.Print("ERROR RETURNING CURRENT WORKING DIRECTORY")
-                }
-                
-                goesToJunk := fmt.Sprint(currDir +"/"+ arg)
-                newDir := fmt.Sprint(junkDir + arg)
-                doesExist := Exists(goesToJunk)
-                
-                if doesExist && hasHomeDir {
-                        os.Rename(goesToJunk, newDir)
-                } else {
-                        err := "File or directory does not exist in the current directory, ya dumb-butt."
-                        fmt.Print(err)
-                }
-        } else {
-                fmt.Print(junkDesc)
-        }
+	a := os.Args
+
+	if len(a) == 2 {
+		moveThings(a[2])
+
+	} else if len(a) > 2 {
+		iterate(a)
+
+	} else {
+		fmt.Println(junkDesc)
+
+	}
 
 }
 
+// Iterates over multiple command line arguments
+func iterate(args []string) {
+	for _, a := range args {
+		moveThings(a)
+
+	}
+
+}
+
+func moveThings(arg string) {
+	// Variables for directory paths
+	currDir, err := os.Getwd()
+	if err != nil {
+		fmt.Print("ERROR RETURNING CURRENT WORKING DIRECTORY")
+	}
+	homeDir, hasHomeDir := os.LookupEnv("HOME")
+	junkDir := fmt.Sprint(homeDir + "/.Junk/")
+	newDir := fmt.Sprint(junkDir + arg)
+	goesToJunk := fmt.Sprint(currDir + "/" + arg)
+
+	// Bool variable to make sure that the file/directory path exists
+	doesExist := exists(goesToJunk)
+
+	if doesExist && hasHomeDir {
+		os.Rename(goesToJunk, newDir)
+
+	} else {
+		err := "File or directory does not exist in the current directory, ya dumb-butt."
+		fmt.Print(err)
+
+	}
+
+}
+
+var junkDesc string = "This is the Junk Command Line Tool! Never 'rm -rf' your files or directories into oblivion ever again! Enter '--help' or '-h' for a list of options"
+
 //Checks whether a file or directory exists
-func Exists(name string) bool {
-    if _, err := os.Stat(name); err != nil {
-    if os.IsNotExist(err) {
-                return false
-            }
-    }
-    return true
+func exists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
 }
